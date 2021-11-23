1381c6c57a3237cc2b0a6bb0ac0f08ce86dbbb2e
Pavel Dubov <PADking888@yandex.ru>
2021-11-23 11:29:48 +0300

    Extract environment variables from .env

--- selfstorage/selfstorage/settings.py
+++ selfstorage/selfstorage/settings.py
@@ -25,12 +25,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-2^#h3(t4koh3#5184g%blx*d2dxi2hr4bq#^#52iasazrh2bu7'
+SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = env.bool('DEBUG')
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = env.list('ALLOWED_HOSTS')
 
 
 # Application definition
