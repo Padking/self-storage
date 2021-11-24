be7a9219a7bb16c908dcb9c9649ea0287dd19790
Pavel Dubov <PADking888@yandex.ru>
2021-11-24 18:07:30 +0300

    Add place addreses form

--- selfstorage/seasonal_keeping/forms.py
+++ selfstorage/seasonal_keeping/forms.py
@@ -1,14 +1,26 @@
 from django import forms
 
+from core.models import Place
 
-CHOICES = [
+
+NUMBER_OF_THINGS_CHOICES = [
     ('1', 'Одна'),
     ('2', 'Две'),
     ('3', 'Три'),
 ]
 
+ADDRESSES_CHOICES = [
+    (place.address, place.address) for place in Place.objects.all()
+]
+
 
 class NumberThingsForm(forms.Form):
     number_of_things = forms.ChoiceField(label='Количество единиц хранения',
-                                         choices=CHOICES,
+                                         choices=NUMBER_OF_THINGS_CHOICES,
                                          widget=forms.RadioSelect)
+
+
+class StorageAddressForm(forms.Form):
+    address = forms.ChoiceField(label='Адрес склада',
+                                choices=ADDRESSES_CHOICES,
+                                widget=forms.RadioSelect)
